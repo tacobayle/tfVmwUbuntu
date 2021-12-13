3b0d4b609a1eceda86ae1da617297369bcc01af3
tacobayle <nicolas.bayle@gmail.com>
2021-12-13 15:50:35 +0000

    20211213

--- variables.tf
+++ variables.tf
@@ -61,7 +61,7 @@ variable "ubuntu" {
     cpu = 24
     if_name = "ens192"
     memory = 65536
-    disk = 98304
+    disk = 128
     wait_for_guest_net_routable = "false"
     netplanFile = "/etc/netplan/50-cloud-init.yaml"
   }
