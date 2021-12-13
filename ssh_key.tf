6b18f3ecbf37290b6b3e78bac80fc0b7712ce1b2
tacobayle <nicolas.bayle@gmail.com>
2021-12-13 16:04:01 +0000

    20211213_clear_ssh_key

--- ssh_key.tf
+++ ssh_key.tf
@@ -9,3 +9,17 @@ resource "local_file" "private_key" {
   filename        = pathexpand("~/.ssh/${var.ssh_key.private_key_name}.pem")
   file_permission = var.ssh_key.file_permission
 }
+
+resource "null_resource" "clear_ssh_key_locally_static" {
+  count = (var.dhcp == false ? length(var.ubuntu_ip4_addresses) : 0)
+  provisioner "local-exec" {
+    command = "ssh-keygen -f \"/home/ubuntu/.ssh/known_hosts\" -R \"${split("/", var.ubuntu_ip4_addresses[count.index])[0]}\" || true"
+  }
+}
+
+resource "null_resource" "clear_ssh_key_locally_dhcp" {
+  count = (var.dhcp == true ? var.ubuntu.count : 0)
+  provisioner "local-exec" {
+    command = "ssh-keygen -f \"/home/ubuntu/.ssh/known_hosts\" -R \"${vsphere_virtual_machine.ubuntu_dhcp[count.index].default_ip_address}\" || true"
+  }
+}
